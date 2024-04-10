264bb26fee82e0f9d0ce1f129c54cee8daafc5b6
Ivoine <ivoinestrachan242@gmail.com>
2024-04-10 15:24:27 -0700

    a bit of doccumenentation setup

---
+++ docs/theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Nexus Suit</span>,
+  project: {
+    link: 'https://github.com/ivoinestrachan/suit-api',
+  },
+  docsRepositoryBase: 'https://github.com/ivoinestrachan/suit-api',
+  footer: {
+    text: 'Nextra Suit Docs',
+  },
+}
+
+export default config
